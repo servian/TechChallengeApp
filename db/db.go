3ff7686128420a1a16a367c31594d02c7987f7c0
Tom <tom@vibrato.com.au>
2018-02-17 12:22:54 +1100

    Added simple database deployment, moved sql into go file as it shouldn't require modification

--- db/db.go
+++ db/db.go
@@ -25,6 +25,8 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/lib/pq"
+
 	_ "github.com/lib/pq"
 	"github.com/vibrato/TechTestApp/model"
 )
@@ -43,6 +45,154 @@ func getDbInfo(cfg Config) string {
 		cfg.DbHost, cfg.DbPort, cfg.DbUser, cfg.DbPassword, cfg.DbName)
 }
 
+// RebuildDb drops the database and recreates it
+func RebuildDb(cfg Config) error {
+	dbinfo := fmt.Sprintf("host=%s port=%s user=%s password=%s sslmode=disable",
+		cfg.DbHost, cfg.DbPort, cfg.DbUser, cfg.DbPassword)
+
+	db, err := sql.Open("postgres", dbinfo)
+
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	query := "DROP DATABASE IF EXISTS " + cfg.DbName
+
+	fmt.Println(query)
+
+	_, err = db.Query(query)
+
+	if err != nil {
+		return err
+	}
+
+	query = fmt.Sprintf(`CREATE DATABASE %s
+WITH 
+OWNER = %s
+ENCODING = 'UTF8'
+LC_COLLATE = 'en_US.utf8'
+LC_CTYPE = 'en_US.utf8'
+TABLESPACE = pg_default
+CONNECTION LIMIT = -1;`, cfg.DbName, cfg.DbUser)
+
+	fmt.Println(query)
+
+	_, err = db.Query(query)
+
+	return err
+}
+
+func CreateTable(cfg Config) error {
+	dbinfo := getDbInfo(cfg)
+
+	db, err := sql.Open("postgres", dbinfo)
+
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	tx, err := db.Begin()
+
+	if err != nil {
+		return err
+	}
+
+	defer tx.Rollback()
+
+	query := "DROP TABLE IF EXISTS tasks CASCADE"
+
+	fmt.Println(query)
+
+	_, err = tx.Exec(query)
+
+	if err != nil {
+		return err
+	}
+
+	query = "CREATE TABLE tasks ( id SERIAL PRIMARY KEY, completed boolean NOT NULL, priority integer NOT NULL, title text NOT NULL)"
+
+	fmt.Println(query)
+
+	_, err = tx.Exec(query)
+
+	if err != nil {
+		return err
+	}
+
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit()
+
+	return err
+}
+
+func SeedData(cfg Config) error {
+
+	dbinfo := getDbInfo(cfg)
+
+	db, err := sql.Open("postgres", dbinfo)
+
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	tx, err := db.Begin()
+
+	if err != nil {
+		return err
+	}
+
+	defer tx.Rollback()
+
+	stmt, err := tx.Prepare(pq.CopyIn("tasks", "completed", "priority", "title"))
+
+	if err != nil {
+		return err
+	}
+
+	tasks := getSeedTasks()
+
+	for _, task := range tasks {
+		_, err = stmt.Exec(task.Complete, task.Priority, task.Title)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = stmt.Exec()
+
+	if err != nil {
+		return err
+	}
+
+	err = stmt.Close()
+
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit()
+
+	return err
+}
+
+func getSeedTasks() []model.Task {
+	var tasks = []model.Task{
+		model.Task{Complete: false, Priority: 0, Title: "1st Task"},
+		model.Task{Complete: false, Priority: 0, Title: "2nd Task"},
+		model.Task{Complete: false, Priority: 0, Title: "3rd Task"},
+	}
+	return tasks
+}
+
 // GetAllTasks lists ass tasks in the database
 func GetAllTasks(cfg Config) ([]model.Task, error) {
 
@@ -58,7 +208,7 @@ func GetAllTasks(cfg Config) ([]model.Task, error) {
 
 	defer db.Close()
 
-	rows, err := db.Query("SELECT * FROM public.\"Tasks\"")
+	rows, err := db.Query("SELECT * FROM tasks")
 
 	if err != nil {
 		return nil, err
@@ -86,7 +236,7 @@ func AddTask(cfg Config, task model.Task) (model.Task, error) {
 
 	defer db.Close()
 
-	err = db.QueryRow("INSERT INTO public.\"Tasks\"(\"Completed\", \"Priority\", \"Title\") VALUES($1, $2, $3) returning \"Id\"",
+	err = db.QueryRow("INSERT INTO tasks (completed, priority, title) VALUES($1, $2, $3) returning id",
 		task.Complete, task.Priority, task.Title).Scan(&task.ID)
 
 	if err != nil {
@@ -107,7 +257,7 @@ func DeleteTask(cfg Config, task model.Task) error {
 
 	defer db.Close()
 
-	stmt, err := db.Prepare("DELETE FROM public.\"Tasks\" WHERE \"Id\"=$1")
+	stmt, err := db.Prepare("DELETE FROM tasks WHERE id=$1")
 
 	if err != nil {
 		return err
@@ -145,7 +295,7 @@ func UpdateTask(cfg Config, task model.Task) (model.Task, error) {
 
 	defer db.Close()
 
-	stmt, err := db.Prepare("UPDATE pulic.\"Tasks\" SET Completed=$1, Priority=$2, Title=$3 WHERE \"Id\"=$4")
+	stmt, err := db.Prepare("UPDATE tasks SET completed=$1, priority=$2, title=$3 WHERE id=$4")
 
 	if err != nil {
 		return task, err
