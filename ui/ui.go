0f1f37082944c26aae8c02858512b03ef1d9a671
Tommy Tang <tommy.tang@servian.com>
2020-01-29 10:45:46 +1100

    copyright 2020

--- ui/ui.go
+++ ui/ui.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 Servian
+// Copyright © 2020 Servian
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
