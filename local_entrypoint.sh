862f88762e2b031d5bde80954480b41abb9d7d85
Rene Brauwers <rene@brauwers.com.au>
2020-06-23 20:10:31 +1000

    Added line to use for when deploying locally as originally only depolyments to dbs in GC, AWS, Azure and the likes where supported (included flag -s as mentioned in the doco, which will skip creating a db)

--- local_entrypoint.sh
+++ local_entrypoint.sh
@@ -2,4 +2,7 @@
 #Wait 10 seconds in order to allow for the database to be up and running.
 sleep 10s;
 #Execute database creation and seeding after which the web application will be served.
-./TechTestApp updatedb -s && ./TechTestApp serve;
+#Use for local development
+./TechTestApp updatedb && ./TechTestApp serve;
+#uncomment when deploying into GC, Azure or AWS; will require however that the database already exists.
+#./TechTestApp updatedb -s && ./TechTestApp serve;
