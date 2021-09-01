f2426d3a6fa0b32b0a36aa6a2019e811a1be79fc
edison.alday <edison.alday@gmail.com>
2021-09-02 02:29:39 +0800

    provision RDS

--- outputs.tf
+++ outputs.tf
@@ -92,6 +92,7 @@ output "techchallenge-asg_target_group_arns" {
   value       = module.techchallenge-asg.autoscaling_group_target_group_arns
 }
 
+#ALB
 output "techchallenge-alb_id" {
   description = "The ID and ARN of the load balancer we created."
   value       = module.techchallenge-alb.lb_id
@@ -135,6 +136,77 @@ output "target_group_arns" {
 output "target_group_attachments" {
   description = "ARNs of the target group attachment IDs."
   value       = module.techchallenge-alb.target_group_attachments
+}
+
+#RDS
+output "db_instance_address" {
+  description = "The address of the RDS instance"
+  value       = module.techchallenge-rds.db_instance_address
+}
+
+output "db_instance_arn" {
+  description = "The ARN of the RDS instance"
+  value       = module.techchallenge-rds.db_instance_arn
+}
+
+output "db_instance_availability_zone" {
+  description = "The availability zone of the RDS instance"
+  value       = module.techchallenge-rds.db_instance_availability_zone
+}
+
+output "db_instance_endpoint" {
+  description = "The connection endpoint"
+  value       = module.techchallenge-rds.db_instance_endpoint
+}
+
+output "db_instance_hosted_zone_id" {
+  description = "The canonical hosted zone ID of the DB instance (to be used in a Route 53 Alias record)"
+  value       = module.techchallenge-rds.db_instance_hosted_zone_id
+}
+
+output "db_instance_id" {
+  description = "The RDS instance ID"
+  value       = module.techchallenge-rds.db_instance_id
+}
+
+output "db_instance_resource_id" {
+  description = "The RDS Resource ID of this instance"
+  value       = module.techchallenge-rds.db_instance_resource_id
+}
+
+output "db_instance_status" {
+  description = "The RDS instance status"
+  value       = module.techchallenge-rds.db_instance_status
+}
+
+output "db_instance_name" {
+  description = "The database name"
+  value       = module.techchallenge-rds.db_instance_name
+}
+
+output "db_instance_port" {
+  description = "The database port"
+  value       = module.techchallenge-rds.db_instance_port
+}
+
+output "db_subnet_group_id" {
+  description = "The db subnet group name"
+  value       = module.techchallenge-rds.db_subnet_group_id
+}
+
+output "db_subnet_group_arn" {
+  description = "The ARN of the db subnet group"
+  value       = module.techchallenge-rds.db_subnet_group_arn
+}
+
+output "db_parameter_group_id" {
+  description = "The db parameter group id"
+  value       = module.techchallenge-rds.db_parameter_group_id
+}
+
+output "db_parameter_group_arn" {
+  description = "The ARN of the db parameter group"
+  value       = module.techchallenge-rds.db_parameter_group_arn
 }
 
 # Security group
